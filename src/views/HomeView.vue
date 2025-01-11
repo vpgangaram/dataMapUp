08808f5ebe64a619090692ab00e915d760cd8220
vpgangaram <gangaramanavar@gmail.com>
2025-01-12 02:08:33 +0530

    added boiuler plate

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,15 @@
 <template>
-  <v-container> 
-    hello
+  <v-container>
+    <dataTable :tableHeaders="tableHeaderData" :tableData="tabledata" />
   </v-container>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+import dataTable from '@/components/dataTable.vue';
+
+const tableHeaderData = ref([]);
+const tabledata = ref([]);
 </script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/boilerPlate.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container>
+
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="scss" scoped></style>
