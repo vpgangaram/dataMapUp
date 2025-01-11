793e1ba5c759705b2cd614f5a687668951428439
vpgangaram <gangaramanavar@gmail.com>
2025-01-11 21:42:26 +0530

    vuetify integration and code refactor

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <v-container> 
+    hello
+  </v-container>
 </template>
+
+<script setup lang="ts">
+</script>
